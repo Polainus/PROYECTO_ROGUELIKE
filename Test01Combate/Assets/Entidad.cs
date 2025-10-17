0c9ca3e9aba706728009c0b65164c46a0ab4c9b0
Polainus <117382192+Polainus@users.noreply.github.com>
2025-10-17 17:06:30 +0200

    Delete Test01Combate directory

--- Test01Combate/Assets/Entidad.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Entidad : MonoBehaviour
-{
-    // Sirve como marcador de que este objeto es una entidad.
-    // Podremos buscar sus componentes con GetComponent<T>().
-}
